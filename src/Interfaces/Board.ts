856964af89a0f130ac29594a7c6c86353b74f3fb
Affonso Xavier <dixavier27@gmail.com>
2024-07-09 19:40:16 -0400

    feat(Interface): create board interface

---
+++ src/Interfaces/Board.ts
@@ -0,0 +1,18 @@
+export interface Board {
+    id: string;
+    name: string;
+    desc?: string;
+    descData?: string | null;
+    closed: boolean;
+    idMemberCreator: string;
+    idOrganization?: string;
+    pinned: boolean;
+    url: string;
+    shortUrl: string;
+    prefs: any;
+    labelNames: { [color: string]: string };
+    starred: boolean;
+    limits: any;
+    memberships: any[];
+    enterpriseOwned: boolean;
+}
