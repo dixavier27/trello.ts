d99b822f0c98023b3437870dc88d44c09c6fdbf3
Affonso Xavier <dixavier27@gmail.com>
2024-07-08 23:24:00 -0400

    refactor(endpoints): create a const to each endpoint

--- src/Trello.ts
+++ src/Trello.ts
@@ -1,4 +1,41 @@
 import dotenv from "dotenv";
+
+import { Action } from './Actions'
+
+import { Applications } from './Applications'
+
+import { Batch } from './Batch'
+
+import { Boards } from './Boards'
+
+import { Cards } from './Cards'
+
+import { Checklists } from './Checklists'
+
+import { CustomFields } from './CustomFields'
+
+import { Emoji } from './Emoji'
+
+import { Enterprises } from './Enterprises'
+
+import { Labels } from './Labels'
+
+import { Lists } from './Lists'
+
+import { Members } from './Members'
+
+import { Notifications } from './Notifications'
+
+import { Organizations } from './Organizations'
+
+import { Plugins } from './Plugins'
+
+import { Search } from './Search'
+
+import { Tokens } from './Tokens'
+
+import { Webhooks } from './Webhooks'
+
 dotenv.config();
 
 export const apiKey = process.env.API_KEY;
