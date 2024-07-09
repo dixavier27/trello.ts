cb53ea0794eb551de782d93cf9d8b864c372c47d
Affonso Xavier <dixavier27@gmail.com>
2024-07-08 22:53:14 -0400

    refactor(build): install dotenv and axios

--- src/Trello.ts
+++ src/Trello.ts
@@ -1,1 +1,10 @@
-console.log('It Works!')
+import dotenv from "dotenv";
+dotenv.config();
+
+export const apiKey = process.env.API_KEY;
+
+export const apiToken = process.env.API_TOKEN;
+
+export const url = 'https://api.trello.com/1'
+
+console.log('It Works! 🚀')
