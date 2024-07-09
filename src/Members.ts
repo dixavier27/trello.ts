6a76b894969579aa28e27cd2bc874fea93c6bb64
Affonso Xavier <dixavier27@gmail.com>
2024-07-09 17:28:03 -0400

    refactor(Members): add operations list

--- src/Members.ts
+++ src/Members.ts
@@ -1,7 +1,21 @@
 import axios from "axios";
 import { apiUrl, apiAuth } from "./Trello";
 import { Board } from "./Interfaces/Board";
-export const Members = {};
+
+// GET /members/{id}
+// PUT /members/{id}
+// GET /members/{id}/{field}
+// GET /members/{id}/actions
+// GET /members/{id}/boardBackgrounds
+// POST /members/{id}/boardBackgrounds
+// GET /members/{id}/boardBackgrounds/{idBackground}
+// PUT /members/{id}/boardBackgrounds/{idBackground}
+// DEL /members/{id}/boardBackgrounds/{idBackground}
+// GET /members/{id}/boardStars
+// POST /members/{id}/boardStars
+// GET /members/{id}/boardStars/{idStar}
+// PUT /members/{id}/boardStars/{idStar}
+// DEL /members/{id}/boardStars/{idStar}
 
 // GET /members/{id}/boards
 export async function getMemberBoards(
@@ -23,3 +37,34 @@ export async function getMemberBoards(
         throw new Error(err);
     }
 }
+
+// GET /members/{id}/boardsInvited
+// GET /members/{id}/cards
+// GET /members/{id}/customBoardBackgrounds
+// POST /members/{id}/customBoardBackgrounds
+// GET /members/{id}/customBoardBackgrounds/{idBackground}
+// PUT /members/{id}/customBoardBackgrounds/{idBackground}
+// DEL /members/{id}/customBoardBackgrounds/{idBackground}
+// GET /members/{id}/customEmoji
+// POST /members/{id}/customEmoji
+// GET /members/{id}/customEmoji/{idEmoji}
+// GET /members/{id}/customStickers
+// POST /members/{id}/customStickers
+// GET /members/{id}/customStickers/{idSticker}
+// DEL /members/{id}/customStickers/{idSticker}
+// GET /members/{id}/notifications
+// GET /members/{id}/organizations
+// GET /members/{id}/organizationsInvited
+// GET /members/{id}/savedSearches
+// POST /members/{id}/savedSearches
+// GET /members/{id}/savedSearches/{idSearch}
+// PUT /members/{id}/savedSearches/{idSearch}
+// DEL /members/{id}/savedSearches/{idSearch}
+// GET /members/{id}/tokens
+// POST /members/{id}/avatar
+// POST /members/{id}/oneTimeMessagesDismissed
+// GET /members/{id}/notificationsChannelSettings
+// PUT /members/{id}/notificationsChannelSettings
+// GET /members/{id}/notificationsChannelSettings/{channel}
+// PUT /members/{id}/notificationsChannelSettings/{channel}
+// PUT /members/{id}/notificationsChannelSettings/{channel}/{blockedKeys}
