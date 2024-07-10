0c9531f1a1a5ecfbbba018492fb1676a4130ea86
Affonso Xavier <dixavier27@gmail.com>
2024-07-10 00:56:13 -0400

    new file:   src/Boards-temp.ts 	modified:   src/Lists.ts 	modified:   src/Trello.ts

--- src/Lists.ts
+++ src/Lists.ts
@@ -1,1 +1,13 @@
 export const Lists = {};
+
+// GET /lists/{id}
+// PUT /lists/{id}
+// POST /lists
+// POST /lists/{id}/archiveAllCards
+// POST /lists/{id}/moveAllCards
+// PUT /lists/{id}/closed
+// PUT /lists/{id}/idBoard
+// PUT /lists/{id}/{field}
+// GET /lists/{id}/actions
+// GET /lists/{id}/board
+// GET /lists/{id}/cards
--- src/Trello.ts
+++ src/Trello.ts
@@ -1,4 +1,6 @@
 import dotenv from "dotenv";
+import { getMemberBoards } from "./Members";
+import { getBoardLists } from "./Boards";
 
 export { Action } from './Actions';
 
@@ -6,9 +8,9 @@ export { Applications } from './Applications'
 
 export { Batch } from './Batch'
 
-export { Boards } from './Boards'
+export { createBoard, createBoardList, deleteBoard, getBoard, getBoardLists, getBoardMemberships, updateBoard } from "./Boards";
 
-export { Cards } from './Cards'
+export { createCard } from "./Cards";
 
 export { Checklists } from './Checklists'
 
@@ -22,7 +24,7 @@ export { Labels } from './Labels'
 
 export { Lists } from './Lists'
 
-export { Members } from './Members'
+export { getMemberBoards } from "./Members";
 
 export { Notifications } from './Notifications'
 
@@ -42,6 +44,44 @@ export const apiKey = process.env.API_KEY;
 
 export const apiToken = process.env.API_TOKEN;
 
-export const url = 'https://api.trello.com/1'
+export const apiUrl = 'https://api.trello.com/1';
 
-console.log('It Works! 🚀')
+export const apiAuth = `key=${apiKey}&token=${apiToken}`;
+
+console.log('🚀 It Works!\n')
+
+getMemberBoards('me', { filter: undefined, fields: 'name' }).then(res => {
+    console.log('\x1b[36m%s\x1b[0m', '✅ getMemberBoards:', res, '\n');
+});
+
+// getBoardMemberships('66884501d7ef1f7f86227287', { filter: 'all', member: true, member_fields: 'username' }).then(res => {
+//     console.log('\x1b[36m%s\x1b[0m', '✅ Members:', res, '\n');
+// });
+
+// getBoard('66884501d7ef1f7f86227287', { fields: 'name' }).then(res => {
+//     console.log('\x1b[36m%s\x1b[0m', '✅ getBoard:', res, '\n');
+// })
+
+// updateBoard('66884501d7ef1f7f86227287', { name: 'Quadro de Teste'}).then(res => {
+//     console.log('\x1b[36m%s\x1b[0m', '✅ updateBoard:', res, '\n');
+// });
+
+// deleteBoard('668d9bf0de01eea4c37ab6c4').then(res => {
+//     console.log('\x1b[36m%s\x1b[0m', '✅ deleteBoard:', true, '\n');
+// });
+
+// createBoard({ name: 'Exemplo de Quadro' }).then(res => {
+//     console.log('\x1b[36m%s\x1b[0m', '✅ createBoard:', res, '\n');
+// });
+
+getBoardLists('66884501d7ef1f7f86227287', { cards: 'all', fields: 'name', card_fields: undefined }).then(res => {
+    console.log('\x1b[36m%s\x1b[0m', '✅ getBoardLists:', res, '\n');
+});
+
+// createBoardList('66884501d7ef1f7f86227287', { name: 'A ser feito' }).then(res => {
+//     console.log('\x1b[36m%s\x1b[0m', '✅ getBoardLists:', res, '\n');
+// });
+
+// createCard({ name: 'Nome Maluco!', idList: '6688450b6e893347ec7e6de6' }).then(res => {
+//     console.log('\x1b[36m%s\x1b[0m', '✅ createCard:', res, '\n');
+// });
